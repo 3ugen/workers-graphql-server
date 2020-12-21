cc85de24bdd23d0d6bf20b0266e08b77838e10e5
Eugen Pyatygin <eugen.pyatygin@gmail.com>
2020-12-21 23:46:17 +0500

    v5 test again v4 forward endpoint v3 restart hasura v2 test for local set hasura GraphQL endpoint

--- src/index.js
+++ src/index.js
@@ -38,25 +38,7 @@ const graphQLOptions = {
 const handleRequest = async request => {
   const url = new URL(request.url)
   try {
-    if (url.pathname === graphQLOptions.baseEndpoint) {
-      const response =
-        request.method === 'OPTIONS'
-          ? new Response('', { status: 204 })
-          : await apollo(request, graphQLOptions)
-      if (graphQLOptions.cors) {
-        setCors(response, graphQLOptions.cors)
-      }
-      return response
-    } else if (
-      graphQLOptions.playgroundEndpoint &&
-      url.pathname === graphQLOptions.playgroundEndpoint
-    ) {
-      return playground(request, graphQLOptions)
-    } else if (graphQLOptions.forwardUnmatchedRequestsToOrigin) {
-      return fetch(request)
-    } else {
-      return new Response('Not found', { status: 404 })
-    }
+    return await apollo(request, graphQLOptions)
   } catch (err) {
     return new Response(graphQLOptions.debug ? err : 'Something went wrong', { status: 500 })
   }
